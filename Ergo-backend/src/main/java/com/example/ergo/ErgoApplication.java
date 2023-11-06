4f4ee082227daefef56242fd097897217a174c2e
d1752172377 <1752472377@qq.com>
2023-11-07 00:47:52 +0800

    修改控制台输出

--- Ergo-backend/src/main/java/com/example/ergo/ErgoApplication.java
+++ Ergo-backend/src/main/java/com/example/ergo/ErgoApplication.java
@@ -12,6 +12,7 @@ public class ErgoApplication {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(ErgoApplication.class, args);
         Environment environment = applicationContext.getBean(Environment.class);
         System.out.println("\n\n ============> 系统启动成功！后台地址：http://localhost:" + environment.getProperty("server.port"));
+        System.out.println(" ============> 巴啦啦能量小魔仙乌卡拉全身变");
     }
 
 }
