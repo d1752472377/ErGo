d742d059bb3c5018b9c8c811287af5b0fd3f8de5
d1752172377 <1752472377@qq.com>
2023-11-12 02:33:24 +0800

    调增

---
+++ vue-frontend/src/views/ArchiveListCell.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="archive-list-cell">
+    <div class="info">
+        <Tag class="time-Tag">{{ post.createTime }}</Tag>
+        <div class="base-info">
+            <router-link :to="{ name: 'article', params: { articleId: post.id } }">
+                {{ post.articleName }}
+            </router-link>
+            <a href="see-desc" @click.stop="seeDesc" :class="{show: showDesc}">
+                查看简介&nbsp;&nbsp;<Icon type="android-arrow-dropup-circle"></Icon></a>
+            <p class="desc" :class="{ show: showDesc }">{{ post.articleRemark }}</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name:"archive-list-cell",
+   props: {
+    post: {
+        Type: Object,
+    }
+   },
+   mixins: [minxin]
+}
+</script>
+
+<style>
+
+</style>
