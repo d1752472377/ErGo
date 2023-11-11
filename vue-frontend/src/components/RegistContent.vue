ec6732dcba7001125f419ae7d9a16c6c2abace5c
d1752172377 <1752472377@qq.com>
2023-11-12 03:50:43 +0800

    Update RegistContent.vue

--- vue-frontend/src/components/RegistContent.vue
+++ vue-frontend/src/components/RegistContent.vue
@@ -2,7 +2,7 @@
     <div class="login">
         <el-form ref="loginForm" :model="loginForm" :rules="loginRules" label-position="left" label-width="0px"
             class="login-form">
-            <h3 class="title">欢迎使用</h3>
+            <h3 class="title">注册</h3>
             <el-form-item prop="username">
                 <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
                     
