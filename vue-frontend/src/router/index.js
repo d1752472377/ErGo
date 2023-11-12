954e825f7dbde834c9f48d6c925df978d245de28
d1752172377 <1752472377@qq.com>
2023-11-12 20:00:23 +0800

    忘记密码

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -5,50 +5,62 @@ import Index from '@/components/index/Index.vue'
 //头和尾
 import SimpleHeader from '@/components/SimpleHeader.vue'
 import CommonFooter from '@/components/CommonFooter.vue'
+//首页
 import HomeContent from '@/components/HomeContent.vue'
 import LoginContentVue from '@/components/LoginContent.vue'
 import RegistContent from '@/components/RegistContent.vue'
+import RetrievePassword from '@/components/RetrievePassword.vue'
 Vue.use(Router)
 export default new Router({
-    mode: 'history',
-    base: __dirname,
-    scrollBehavior: () => ({ y: 0 }),
-    routes: [{
-        path: '/',
-        name: 'Home',
-        redirect: "home",
-        component: Index,
-        children: [{
-                path: '/',
-                name: 'home',
-                components: {
-                    header: SimpleHeader,
-                    content: HomeContent,
-                    footer: CommonFooter
-                }
-        },
-        //登录页
-        {
-          path: '/login',
-          name: 'login',
-          components: {
-              header: SimpleHeader,
-              content: LoginContentVue,
-              footer: CommonFooter
-          }
-      },
-      //注册页
-      {
-        path: '/regist',
-        name: 'regist',
-        components: {
-            header: SimpleHeader,
-            content: RegistContent,
-            footer: CommonFooter
-        }
+  mode: 'history',
+  base: __dirname,
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [{
+    path: '/',
+    name: 'Home',
+    redirect: "home",
+    component: Index,
+    children: [{
+      path: '/',
+      name: 'home',
+      components: {
+        header: SimpleHeader,
+        content: HomeContent,
+        footer: CommonFooter
+      }
     },
-      ]
-    }]
+    //登录页
+    {
+      path: '/login',
+      name: 'login',
+      components: {
+        header: SimpleHeader,
+        content: LoginContentVue,
+        footer: CommonFooter
+      }
+    },
+    //注册页
+    {
+      path: '/regist',
+      name: 'regist',
+      components: {
+        header: SimpleHeader,
+        content: RegistContent,
+        footer: CommonFooter
+      }
+    },
+    //修改密码
+    {
+      path: '/recover',
+      name: 'recover',
+      components: {
+        header: SimpleHeader,
+        content: RetrievePassword,
+        footer: CommonFooter
+      }
+    },
+    ]
+  }]
 
 })
 const routes = [
