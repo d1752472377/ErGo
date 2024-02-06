99b052406b3d42bf35c8ef607a8f7a01248dea1d
d1752172377 <1752472377@qq.com>
2024-02-07 01:05:58 +0800

    优化评论为二级评论

--- Ergo-backend/src/main/java/com/example/ergo/service/impl/CommentServiceImpl.java
+++ Ergo-backend/src/main/java/com/example/ergo/service/impl/CommentServiceImpl.java
@@ -8,9 +8,9 @@ import com.example.ergo.vo.dto.CommentDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.LinkedList;
+import java.util.List;
 /**
  * 评论表(Comment)表服务实现类
  *
@@ -22,34 +22,61 @@ public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment> impl
 
     @Autowired
     private CommentMapper commentMapper;
+
     @Override
     public List<CommentDto> getCommentList(int articleId, int pageNum, int pageSize) {
-        List<CommentDto> commentVos = this.commentMapper.getCommentList(articleId,pageNum,pageSize);
+        List<CommentDto> commentVos = commentMapper.getCommentList(articleId, pageNum, pageSize);
         List<CommentDto> sons = getSons(commentVos);
+        getParent(sons);
         return sons;
     }
-    private String getParentName(Integer id){
-        return this.commentMapper.getParentName(id);
-    }
-    private List<CommentDto> getSons(Integer parentId){
-        return this.commentMapper.listTreeComment(parentId);
+
+    private String getParentName(Integer id) {
+        return commentMapper.getParentName(id);
     }
 
-    private List<CommentDto> getSons(List<CommentDto> parents){
-        if(parents == null || parents.size() == 0){
+    private List<CommentDto> getSons(Integer parentId) {
+        return commentMapper.listTreeComment(parentId);
+    }
+
+    private List<CommentDto> getSons(List<CommentDto> parents) {
+        if (parents == null || parents.isEmpty()) {
             return null;
         }
         for (CommentDto parent : parents) {
             int parentId = parent.getId();
             int getParentId = parent.getParentCommentId();
             List<CommentDto> sonCommentVos = getSons(parentId);
-            //递归找子评论
             parent.setReplyComments(getSons(sonCommentVos));
             parent.setParentName(getParentName(getParentId));
         }
         return parents;
     }
 
+    private void getParent(List<CommentDto> rawComments) {
+        for (CommentDto topComment : rawComments) {
+            LinkedList<CommentDto> comments = new LinkedList<>();
+            List<CommentDto> replyCmtsByTopCmt = topComment.getReplyComments();
+            if (replyCmtsByTopCmt != null) {
+                for (CommentDto replyComment : replyCmtsByTopCmt) {
+                    handleChild(replyComment, comments, topComment.getUserName());
+                }
+            }
+            topComment.setReplyComments(comments);
+        }
+    }
 
+    private void handleChild(CommentDto replyComment, List<CommentDto> parent, String parentName) {
+        List<CommentDto> grandchildren = replyComment.getReplyComments();
+        replyComment.setReplyComments(null);
+        parent.add(replyComment);
+        if (grandchildren != null) {
+            for (CommentDto grandChild : grandchildren) {
+                grandChild.setParentName(replyComment.getUserName());
+                if (grandChild != null) {
+                    handleChild(grandChild, parent, parentName);
+                }
+            }
+        }
+    }
 }
-
