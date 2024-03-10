c30f6a2fc5df9938580de94154862761b1cab1f4
d1752172377 <1752472377@qq.com>
2024-03-11 03:56:57 +0800

    优化时间显示

--- vue-frontend/src/components/Article/ArticleContent.vue
+++ vue-frontend/src/components/Article/ArticleContent.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="blog-page">
     <div class="left-section">
-     <Card>
-      <div class="blog-detail" v-for="article in blog " :key="article.id">
-        <h1>{{ article.title }}</h1>
-        <div class="author-info">
-          <img :src="article.photo" alt="Author Avatar" />
-          <span>{{ article.userName }}</span>
-          <span>{{ article.createTime }}</span>
-          <!-- <span>阅读量: {{ article.readCount }}</span> -->
+      <Card>
+        <div class="blog-detail" v-for="article in blog " :key="article.id">
+          <h1>{{ article.title }}</h1>
+          <div class="author-info">
+            <img :src="article.photo" alt="Author Avatar" />
+            <span style="margin-left: 5px;">{{ article.userName }}</span>
+            <span class="publish-time" style="margin-left:20px;">
+              <Icon type="ios-timer-outline" class="icon" />于 {{
+                article.createTime | formatDate
+              }} 发布
+            </span>
+            <!-- <span>阅读量: {{ article.readCount }}</span> -->
+          </div>
+          <div class="content" v-html="article.content"></div>
+
         </div>
-        <div class="content" v-html="article.content "></div>
-        
-      </div>
-     </Card>
+      </Card>
       <div class="comment">
         <Card>
-        <CommentList></CommentList>
-      </Card>
+          <CommentList></CommentList>
+        </Card>
       </div>
     </div>
     <div class="right-section">
-        <div v-for="article in blog" :key="article.id">
-          <Author 
-          :userId="article.userId"
-          :userName="article.userName"
-          :avatar="article.photo"
-          ></Author>
-        </div>
+      <div v-for="article in blog" :key="article.id">
+        <Author :userId="article.userId" :userName="article.userName" :avatar="article.photo"></Author>
+      </div>
       <el-card>
         <recommend></recommend>
       </el-card>
@@ -52,7 +52,7 @@ export default {
     Author: AuthorCard,
     CommentList,
     Card
-},
+  },
   data() {
     return {
       blog: {}
@@ -73,31 +73,50 @@ export default {
       };
       getArticleInfo(params).then(res => {
         this.blog = res.data.data.info;
-      }).catch(error=>{
+      }).catch(error => {
         if (error.response) {
-        const status = error.response.status;
-        if (status === 404) {
-          // 处理404错误
-          this.$router.push({ path: '/404' });
-        } else if (status === 400) {
-          // 处理400错误
-          // 例如，显示一个错误消息
-          this.$router.push({ path: '/404' });
-        } else if (status === 403) {
-          // 处理403错误
-          // 例如，显示一个权限不足的错误消息
-          console.error('权限不足');
+          const status = error.response.status;
+          if (status === 404) {
+            // 处理404错误
+            this.$router.push({ path: '/404' });
+          } else if (status === 400) {
+            // 处理400错误
+            // 例如，显示一个错误消息
+            this.$router.push({ path: '/404' });
+          } else if (status === 403) {
+            // 处理403错误
+            // 例如，显示一个权限不足的错误消息
+            console.error('权限不足');
+          } else {
+            // 处理其他错误
+            this.$router.push({ path: '/404' });
+          }
         } else {
-          // 处理其他错误
-          this.$router.push({ path: '/404' });
+          // 处理没有响应的情况
+          console.error('请求出错:', error);
         }
-      } else {
-        // 处理没有响应的情况
-        console.error('请求出错:', error);
-      }
       })
     }
-  }
+  },
+  filters: {
+    formatDate: function (value) {
+      if (value != null) {
+        let date = new Date(value);
+        let y = date.getFullYear();
+        let MM = date.getMonth() + 1;
+        MM = MM < 10 ? "0" + MM : MM;
+        let d = date.getDate();
+        d = d < 10 ? "0" + d : d;
+        let h = date.getHours();
+        h = h < 10 ? "0" + h : h;
+        let m = date.getMinutes();
+        m = m < 10 ? "0" + m : m;
+        let s = date.getSeconds();
+        s = s < 10 ? "0" + s : s;
+        return y + "-" + MM + "-" + d + " " + h + ":" + m + ":" + s;
+      }
+    },
+  },
 };
 </script>
 
@@ -106,16 +125,20 @@ export default {
   width: 1260px;
   display: flex;
   justify-content: space-between;
-  margin: 15px auto; /* 设置上下边距为 15px，左右自动居中 */
+  margin: 15px auto;
+  /* 设置上下边距为 15px，左右自动居中 */
 }
 
 .left-section {
-  width: 75%; /* 调整左侧宽度 */
+  width: 75%;
+  /* 调整左侧宽度 */
 }
 
 .right-section {
-  width: 25%; /* 调整右侧宽度 */
-  margin-left: 20px; /* 添加左右间距 */
+  width: 25%;
+  /* 调整右侧宽度 */
+  margin-left: 20px;
+  /* 添加左右间距 */
 }
 
 .blog-detail {
@@ -125,7 +148,7 @@ export default {
 
 .author-info {
   width: 800px;
-  margin:0 auto;
+  margin: 0 auto;
   margin-top: 10px;
   display: flex;
   align-items: center;
@@ -140,40 +163,56 @@ export default {
 
 .content {
   width: 840px;
-  margin:0 auto;
+  margin: 0 auto;
   font-size: 16px;
   line-height: 1.6;
-  max-width: 100%; /* 限制内容宽度不超过父容器 */
+  max-width: 100%;
+  /* 限制内容宽度不超过父容器 */
   text-align: left;
 }
 
 .content img {
-  max-width: 100%; /* 限制图片宽度不超过父容器 */
-  height: auto; /* 保持宽高比例 */
+  max-width: 100%;
+  /* 限制图片宽度不超过父容器 */
+  height: auto;
+  /* 保持宽高比例 */
 }
+
 .content li {
   list-style-type: none;
   line-height: 1.5;
   margin-bottom: 10px;
 }
+
 /* 设置标题样式 */
-.content h1,.content h2,.content h3,.content h4 {
-  color: #333; /* 设置标题颜色，可以根据需要调整 */
-  font-weight: bold; /* 设置标题粗细，可以根据需要调整 */
-  margin-top: 20px; /* 设置上边距 */
-  margin-bottom: 10px; /* 设置下边距 */
-  
+.content h1,
+.content h2,
+.content h3,
+.content h4 {
+  color: #333;
+  /* 设置标题颜色，可以根据需要调整 */
+  font-weight: bold;
+  /* 设置标题粗细，可以根据需要调整 */
+  margin-top: 20px;
+  /* 设置上边距 */
+  margin-bottom: 10px;
+  /* 设置下边距 */
+
 }
 
 /* 添加虚线效果 */
-.content h1:after,h2:after {
+.content h1:after,
+h2:after {
   content: '';
   display: block;
-  border-bottom: 1px dashed #ccc; /* 设置虚线颜色，可以根据需要调整 */
-  margin-top: 5px; /* 设置虚线与标题之间的距离，可以根据需要调整 */
-  margin-bottom: 15px; /* 设置虚线底部间距，可以根据需要调整 */
+  border-bottom: 1px dashed #ccc;
+  /* 设置虚线颜色，可以根据需要调整 */
+  margin-top: 5px;
+  /* 设置虚线与标题之间的距离，可以根据需要调整 */
+  margin-bottom: 15px;
+  /* 设置虚线底部间距，可以根据需要调整 */
 }
-.comment{
+
+.comment {
   margin-top: 10px;
-}
-</style>
+}</style>
